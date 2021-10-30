8f99ee3a45d156878bce7e7ed853d8f3183c5fcf
unknown <you@example.com>
2021-10-30 15:34:18 +0700

    dacs2

---
+++ vd1/index.php
@@ -0,0 +1,14 @@
+<?php
+$first_name = "Student ";
+$last_name = " house";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p>First name: <?php echo $first_name; ?> </p>
+<p>Lats name : <?php echo $last_name; ?></p>
+</body>
+</html>
